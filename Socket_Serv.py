212dea02cb5fdae076dc3ac8c4b84b7c0418f06c
Hugo Silva <hugo.silva08@hotmail.com>
2019-10-22 23:38:25 -0300

    Adicionada a função POST, com alguns ajustes a fazer ainda

--- Socket_Serv.py
+++ Socket_Serv.py
@@ -3,21 +3,31 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost) Só comunica com o próprio host
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
+
 def method_GET(arquivo):
     try:
-        cabecalho = ''
+        cabecalho = 'HTTP/1.1 200 OK\n'
         body = open(arquivo).read()                    #Tentando abrir arquivo e tentar enviar por meio do socket como string
-        print('Enviado: ', body.encode('utf-8'))
-        conn.send(body.encode('utf-8'))                #Codificando uma string para bytes
+        print('Enviado: \n',cabecalho.encode('utf-8'), body.encode('utf-8'))
+        conn.send(cabecalho.encode('utf-8'), body.encode('utf-8'))                #Codificando uma string para bytes
         
     except:
+        cabecalho = 'HTTP/1.1 404 NOT FOUND'
         body = 'FILE NOT FOUND 404'
-        print('Enviado ',body.encode('utf-8'))
+        print('Enviado :\n',cabecalho, body.encode('utf-8'),)
         conn.send(body.encode('utf-8'))
 
-def method_POST(arquivo):
-    #print('Nada ainda')
-
+def method_POST(source, content):
+    try:
+        arquivo = open(source, 'w')
+        arquivo.write(content) 
+        arquivo.close() 
+        cabecalho = 'HTTP/1.1 200 OK\n'
+        conn.send(cabecalho.encode('utf-8'))
+    except:
+        cabecalho = 'HTTP/1.1 400 ERROR'
+        conn.send(cabecalho.encode('utf-8'))
+    
 
 while True:
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -39,12 +49,13 @@ while True:
                     method = array[0]
                     source = array[1]
                     conection = array[2]
+                    body = array[1]
 
                     if method == 'GET':
                         method_GET(source)
                     
                     if method == 'POST':
-                        method_POST(source)
+                        method_POST(source, body)
 
         finally:
             s.close()
--- VarreString.py
+++
@@ -1,5 +0,0 @@
-pos = 0
-request = "GET /docs/index.html HTTP/1.1"
-pos = pos + request.find(" ")
-method = request[0:pos]
-print(method)
